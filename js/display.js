3dc1ef04ced54f9845b931d7ca71a963dfc80091
Bruno Fernandes <brunofrdsdev@gmail.com>
2026-01-29 17:59:58 +0100

    Refactor displayCard, add resetGameDisplay, and update drawCard/shuffle logic

--- js/display.js
+++ js/display.js
@@ -1,3 +1,20 @@
 export const displayCard = (container, card) => {
-  container.textContent += `${card.suit}${card.value}`
+  const cardDiv = document.createElement("div")
+  cardDiv.classList.add("card")
+
+  cardDiv.textContent = `${card.suit} ${card.value}`
+
+  if (card.suit === "♥" || card.suit === "♦") {
+    cardDiv.style.color = "red"
+  }
+
+  container.appendChild(cardDiv)
+}
+
+const resetDisplay = (container) => {
+  container.innerHTML = ""
+}
+
+export const resetGameDisplay = (containers) => {
+  containers.forEach((container) => resetDisplay(container))
 }
--- js/game.js
+++ js/game.js
@@ -1,9 +1,9 @@
 import { shuffleDeck } from "./deck.js"
-import { displayCard } from "./display.js"
+import { displayCard, resetGameDisplay } from "./display.js"
 
-const drawCard = () => {
-  const card = shuffleDeck().shift()
-  return card
+
+const drawCard = (deck) => {
+  return deck.shift()
 }
 
 export const initGame = () => {
@@ -11,15 +11,22 @@ export const initGame = () => {
 
   const playerContainer = document.querySelector("#player-cards .cards-container")
   const dealerContainer = document.querySelector("#dealer-cards .cards-container")
+
+
   newBtn.addEventListener("click", () => {
+    const containers = [playerContainer, dealerContainer];
+
+    resetGameDisplay(containers)
+
+    let deck = shuffleDeck()
+
     const sequence = [playerContainer, dealerContainer, playerContainer, dealerContainer]
 
     sequence.forEach((container, index) => {
-    setTimeout(() => {
-      const card = drawCard()
-      displayCard(container, card)
-    }, 1000 + (index * 1000));
-  })
+      setTimeout(() => {
+        const card = drawCard(deck)
+        displayCard(container, card)
+      }, 1000 + (index * 1000));
+    })
   })
 }
-
